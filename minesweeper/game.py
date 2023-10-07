c75f22594f0ad0b231fe9b9024a5b6d69bc21d98
killian <killian.carvalho@protonmail.com>
2023-10-07 19:36:47 +0200

    feat: define chronology in start game function

---
+++ minesweeper/game.py
@@ -0,0 +1,22 @@
+import sys
+import config
+import board
+import input_handler
+import display
+
+def start_game():
+    # say hello
+    display.print_welcome()
+    display.start_menu()
+    user_action = input_handler.get_user_action()
+    if user_action == 1:
+        # init game
+        pass
+    elif user_action == 2:
+        # charge game
+        pass
+    else:
+        sys.exit()
+    display.print_difficulty()
+    user_diff = input_handler.get_user_difficulty()
+
