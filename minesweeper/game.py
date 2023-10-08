83421f431387ec3808a37825b030535e578729ab
killian <killian.carvalho@protonmail.com>
2023-10-08 09:43:23 +0200

    feat: the user can now perfectly choose an option for start game and choose difficulty

--- minesweeper/game.py
+++ minesweeper/game.py
@@ -7,18 +7,39 @@ import display
 def start_game():
     # say hello
     display.print_welcome()
-    display.start_menu()
-    user_action = input_handler.get_user_action()
-    if user_action == 1:
-        # init game
-        pass
-    elif user_action == 2:
-        # charge game
-        pass
-    else:
-        sys.exit()
-    display.print_difficulty()
-    user_diff = input_handler.get_user_difficulty()
+
+    start = False
+    while not start:
+        user_action = input_handler.get_int_user(display.start_menu())
+        if user_action == 1:
+            start = True
+            valid_diff = False
+            while not valid_diff:
+                difficulty_choice = input_handler.get_int_user(display.print_difficulty())
+                if difficulty_choice == 1:
+                    grid_size = config.EASY_GRID_SIZE
+                    nbr_mines = config.EASY_NUM_MINES
+                    valid_diff = True
+                elif difficulty_choice == 2:
+                    grid_size = config.MEDIUM_GRID_SIZE
+                    nbr_mines = config.MEDIUM_NUM_MINES
+                    valid_diff = True
+                elif difficulty_choice == 3:
+                    grid_size = config.HARD_GRID_SIZE
+                    nbr_mines = config.HARD_NUM_MINES
+                    valid_diff = True
+                elif difficulty_choice == 4:
+                    print("Sorry, currently doesn't works. Wait the next version.")
+                else:
+                    print("It's not a choice option")
+        elif user_action == 2:
+            # charge game
+            print("Sorry, the load functionality is for another version.")
+        elif user_action == 3:
+            sys.exit()
+        else:
+            print("It's not a choice option")
+
 
 if __name__ == "__main__":
     start_game()
