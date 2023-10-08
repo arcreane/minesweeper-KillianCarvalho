8a9989ca75cc5e3410ce1aaef931fd0bcf13448c
killian <killian.carvalho@protonmail.com>
2023-10-08 21:03:17 +0200

    feat: add the function to get user action - can be upgrade

--- minesweeper/input_handler.py
+++ minesweeper/input_handler.py
@@ -1,6 +1,12 @@
 def get_user_action():
-    return None
-
+    coord_x = get_int_user("Choose a X → coordinate : ")
+    coord_y = get_int_user("Choose a Y ↑ coordinate : ")
+    action = get_int_user("Choose an action:\n\t1- returned\n2- flagged")
+    if action == 1:
+        action = 'R'
+    elif action == 2:
+        action = 'F'
+    return coord_x, coord_y, action
 
 def get_int_user(request):
     print(request)
