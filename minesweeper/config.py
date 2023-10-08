6643aefeadce31276e9e8164f7b1e88ec899764c
killian <killian.carvalho@protonmail.com>
2023-10-08 09:40:45 +0200

    feat: add the constant in config.py - mines and grid size

---
+++ minesweeper/config.py
@@ -0,0 +1,11 @@
+# Beginner difficulty
+EASY_GRID_SIZE = (9, 9)
+EASY_NUM_MINES = 10
+
+# Intermediate difficulty
+MEDIUM_GRID_SIZE = (16, 16)
+MEDIUM_NUM_MINES = 40
+
+# Expert difficulty
+HARD_GRID_SIZE = (16, 30)
+HARD_NUM_MINES = 99
