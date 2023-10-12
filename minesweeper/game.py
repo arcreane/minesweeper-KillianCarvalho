1b4e4a22f9db757a9d046ff4b22e630537bf0a83
killian <killian.carvalho@protonmail.com>
2023-10-12 11:34:21 +0200

    feat: add get_valid_difficulty function - add doctype

--- minesweeper/game.py
+++ minesweeper/game.py
@@ -4,7 +4,30 @@ import board as board_controller
 import input_handler
 import display
 
+def get_valid_difficulty(input_handler, display):
+    difficulty_choice = input_handler.get_int_user(display.print_difficulty())
+    if 1 <= difficulty_choice <= 3:
+        return difficulty_choice
+    elif difficulty_choice == 4:
+        print("Sorry, it currently doesn't work. Wait for the next version.")
+    else:
+        print("It's not a valid option")
+
+    return get_valid_difficulty(input_handler, display)
+
 def start_game():
+    """
+    Starts the Minesweeper game.
+
+    This function displays the welcome message, allows the user to choose an action to start the game
+    (e.g., start a new game, load a saved game, or exit the game), and choose the game difficulty.
+
+    The function handles user input and initializes the game board accordingly.
+
+    Returns:
+        None
+    """
+
     # say hello
     display.print_welcome()
 
@@ -12,17 +35,10 @@ def start_game():
     start = False
     while not start:
         user_action = input_handler.get_int_user(display.start_menu())
+        # 1->  start the game # 2-> charge game # 3 -> exit
         if user_action == 1:
             start = True
-            valid_diff = False
-            while not valid_diff:
-                difficulty_choice = input_handler.get_int_user(display.print_difficulty())
-                if 1 <= difficulty_choice <= 3:
-                    valid_diff = True
-                elif difficulty_choice == 4:
-                    print("Sorry, currently doesn't works. Wait the next version.")
-                else:
-                    print("It's not a choice option")
+            difficulty_choice = get_valid_difficulty(input_handler, display)
             board = board_controller.initialize_board(config.CONFIG_DIFFICULTY[difficulty_choice - 1][0], config.CONFIG_DIFFICULTY[difficulty_choice - 1][1])
             play_game(board)
         elif user_action == 2:
@@ -34,6 +50,25 @@ def start_game():
             print("It's not a choice option")
 
 def play_game(board):
+    """
+        Function to play a game on a given board.
+
+        Args:
+        board (list): A list representing the game state.
+
+        Returns:
+        None
+
+        The function displays the board and then starts a game loop. Players can take actions by entering commands
+        via input_handler.get_user_action(), and the game continues until a player wins or a bomb explodes.
+
+        If a player steps on a bomb, the game ends and displays a failure message. If the player successfully achieves
+         the game's objective (possibly defined in board_controller.has_win()), the game ends with a success message.
+
+        Example usage:
+        board = initialize_board()
+        play_game(board)
+    """
     game_over = False
     display.display_board(board)
     while not game_over:
